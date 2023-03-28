e54602e2fd03088902516eb1d3acb79aa7a5ca94
yerimechoi <yerimechoi@gmail.com>
2023-03-27 21:21:00 -0400

    Updates

---
+++ precache-manifest.20b2d983f63b39ebeee9b0a1ad4fb2bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "21a2f9bb11dc0ba34a86",
+    "url": "/react-portfolio_c20/static/css/main.2010bab1.chunk.css"
+  },
+  {
+    "revision": "21a2f9bb11dc0ba34a86",
+    "url": "/react-portfolio_c20/static/js/main.21a2f9bb.chunk.js"
+  },
+  {
+    "revision": "9f799b83c7635e4a3ba5",
+    "url": "/react-portfolio_c20/static/css/1.abf7f6d7.chunk.css"
+  },
+  {
+    "revision": "9f799b83c7635e4a3ba5",
+    "url": "/react-portfolio_c20/static/js/1.9f799b83.chunk.js"
+  },
+  {
+    "revision": "2555e3d61e041c4e46af",
+    "url": "/react-portfolio_c20/static/js/runtime~main.2555e3d6.js"
+  },
+  {
+    "revision": "6fc71b6e5f3b4a6893bcc594c27670fd",
+    "url": "/react-portfolio_c20/static/media/evie.6fc71b6e.jpg"
+  },
+  {
+    "revision": "f3c9232dece4a8bb8481f879b28cdebb",
+    "url": "/react-portfolio_c20/static/media/classicwhipz.f3c9232d.jpg"
+  },
+  {
+    "revision": "354877b763aac75fbe5bae22e5477c98",
+    "url": "/react-portfolio_c20/static/media/untitled.354877b7.jpg"
+  },
+  {
+    "revision": "f4ccc5ba98637700beeea86c06c2020f",
+    "url": "/react-portfolio_c20/index.html"
+  }
+];
