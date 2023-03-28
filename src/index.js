59f51470d1df83b8ca33be74e5abb40b52602465
yerimechoi <yerimechoi@gmail.com>
2023-03-27 21:14:01 -0400

    gh-pages created to deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+var ghpages = require('gh-pages');
+ghpages.publish ('dist', function(err) {});
+
+ghpages.publish(dir, callback);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
